f1b307ec98353697099dd55e3e5fbd92977dc15f
신동준 <sheys@hanmail.net>
2025-11-06 16:25:54 +0900

    flipfix2

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-// Google Drive PDF Library 설정
 window.DRIVE_CONFIG = {
   API_KEY: "AIzaSyA9T4-2vJq4voyMoVp3KKmc_8isv2OgB6c",
-  FOLDER_IDS: [
-    "1LMHsM-pAAYtbUDyhUQKIGn1l-h0LRS5Q"
-  ],
+  FOLDER_IDS: ["1LMHsM-pAAYtbUDyhUQKIGn1l-h0LRS5Q"],
   PAGE_SIZE: 1000,
   USE_THUMBNAIL: true
 };
